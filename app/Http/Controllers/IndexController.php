d7db117bc8e0e6d375e88f5dde38727933f4a72b
jhonny vasquez <jhonnyvanckruz@gmail.com>
2016-01-31 17:45:27 -0430

    Ultimo cambio

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 #use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Contactos;
+use Mail;
 
 class IndexController extends Controller
 {
@@ -32,23 +33,30 @@ class IndexController extends Controller
 			"imagen"        => ($request->file('imagen') !== null ? $request->file('imagen')->getClientOriginalName() : null)
 		]);
 
+/*		
+$nombre        = $request->all()['nombre'];
+$apellido      = $request->all()['apellido'];
+$email         = $request->all()['email'];
+$pais          = $request->all()['pais'];
+$tipo_contacto = ($request->all()['tipo_contacto'] !== null ? $request->all()['tipo_contacto'] : null);
+$mensaje       = $request->all()['mensaje'];*/
 
-		$html_mensaje  = "<div style='padding:1em;'>";
-		$html_mensaje .= "<h1>".$request->all()['nombre']." ".$request->all()['apellido']."</h1>";
-		$html_mensaje .= "<p style='font-weight:bold;'>Email: ".$request->all()['email']."</p>";
-		$html_mensaje .= "<p style='font-weight:bold;'>Pais: ".$request->all()['pais']."</p>";
-		$html_mensaje .= "<p style='font-weight:bold;'>Tipo de Contacto: ".($request->all()['tipo_contacto'] !== null ? $request->all()['tipo_contacto'] : null)."</p>";
-		$html_mensaje .= "<p style='font-weight:bold;'>Mensaje:</p>";
-		$html_mensaje .= "<p style='padding:1em;text-align:justify;'> ".$request->all()['mensaje']."</p>";
-		$html_mensaje .= "</div>";		
+		$data = array(
+			'nombre'        => $request->all()['nombre'],
+			'apellido'      => $request->all()['apellido'],
+			'email'         => $request->all()['email'],
+			'pais'          => $request->all()['pais'],
+			'tipo_contacto' => ($request->all()['tipo_contacto'] !== null ? $request->all()['tipo_contacto'] : null),
+			'mensaje'       => $request->all()['mensaje'],
+		);
 
+		 Mail::send("mails.contacto",$data, function($mensaje) {
+		    $mensaje->
+		    to("jhonnyvanckruz@gmail.com", "Jhonny Vasquez")->
+		    subject("Nuevo mensaje de Contacto en tu Página web"); 
+		  });
 		
     	return redirect("/");
-		/*
-		echo "<pre>";
-		#print_r($var["nombre"]);
-		print_r($request->all()["nombre"]);
-		echo "</pre>";*/
 
 	}    
 
---
+++ resources/views/mails/contacto.blade.php
@@ -0,0 +1,19 @@
+<div style='padding:1em;'>";
+<h1>{{$nombre}} {{$apellido}}</h1>
+
+<p style='font-weight:bold;'>
+	Email: {{$email}}
+</p>
+
+<p style='font-weight:bold;'>
+	Pais: {{$pais}}
+</p>
+
+<p style='font-weight:bold;'>
+	Tipo de Contacto: {{$tipo_contacto}}</p>
+
+<p style='font-weight:bold;'>Mensaje:</p>
+<p style='padding:1em;text-align:justify;'> 
+	 {{$mensaje}}
+</p>
+</div>	
